dc65a36a71dc7c6769f3f7d5905e8a1d89dab7c4
Anand Ravindran <anandravindran123@gmail.com>
2025-04-07 22:11:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title('Gelada Classifier')
 
 st.info("This is a Gelada classifier Machine Learning application!")
+
+image_uploaded = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
